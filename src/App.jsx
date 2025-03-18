03996236050e69df4cbcf6badf888a214cd246c9
asude-demirci <asudemrci@gmail.com>
2025-03-18 23:12:12 +0300

    feat: add authentication and protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
 import React from 'react';
-import './index.css';
+import { Route, Switch } from 'react-router-dom';
+import { AuthContextProvider } from './contexts/AuthContext';
 import LoginForm from './components/LoginForm';
 import Header from './components/Header';
 import FriendsList from './components/FriendsList';
 import AddFriend from './components/AddFriend';
+import PrivateRoute from './components/PrivateRoute';
+import './index.css';
+
+
 
 function App() {
   return (
+    <AuthContextProvider>
     <div className="App">
       <Header />
-      <LoginForm />
+      <Switch>
+        <Route path="/login" component={LoginForm} />
+        <PrivateRoute path="/friends/add" component={AddFriend} />
+        <PrivateRoute path="/friends" component={FriendsList} />
+        <PrivateRoute path="/" exact component={FriendsList} />
+      </Switch>
     </div>
+    </AuthContextProvider>
   );
 }
 
